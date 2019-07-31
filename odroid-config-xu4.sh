981d849bab33d0b1001bdf51b3a01b5911e5b222
Hyeonki Hong <hhk7734@gmail.com>
2019-07-31 17:45:09 +0900

    Added enable spi option
    Change-Id: I28cc72ea7f58384b2db4a5878608e5942a35f30a

--- odroid-config-xu4.sh
+++ odroid-config-xu4.sh
@@ -1,1 +1,21 @@
 #!/bin/sh
+
+do_spi()
+{
+    DEFAULT=--defaultno
+    if lsmod | grep -q spi; then
+        DEFAULT=
+    fi
+
+    whiptail --yesno "Would you like the SPI interface to be enabled?" $DEFAULT 20 60
+    BUTTON=$?
+    if [ $BUTTON -eq 0 ]; then
+        modprobe spidev
+        modprobe spi_s3c64xx
+        whiptail --msgbox "The SPI interface is enabled" 20 60
+    elif [ $BUTTON -eq 1 ]; then
+        modprobe -r spidev
+        modprobe -r spi_s3c64xx
+        whiptail --msgbox "The SPI interface is disabled" 20 60
+    fi
+}
