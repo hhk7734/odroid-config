0dc426ba235615fba5671ff2f75d7449b2cee156
Hyeonki Hong <hhk7734@gmail.com>
2019-08-08 17:57:39 +0900

    Add C1, C2 support
    Change-Id: Id68601698393a78b654026c7f322f2cb5bb9cdf8

--- install.sh
+++ install.sh
@@ -5,12 +5,24 @@ if [ "$(id -u)" -ne 0 ]; then
     exit 1
 fi
 
+apt update
+
+if [ -z "$(command -v fdtput 2>/dev/null)" ]; then
+    apt install -y device-tree-compiler
+fi
+
 SCRIPT_PATH=$(dirname "$(realpath "$0")")
 
 if [ ! -e /usr/lib/odroid-config ]; then
     mkdir -p /usr/lib/odroid-config
 fi
 cp "$SCRIPT_PATH"/odroid-config-*.sh /usr/lib/odroid-config/
+chmod +x /usr/lib/odroid-config/*.sh
 
 cp "$SCRIPT_PATH"/odroid-config /usr/bin/odroid-config
 chmod +x /usr/bin/odroid-config
+
+cp "$SCRIPT_PATH"/odroid-config-settings.service /etc/systemd/system/odroid-config-settings.service
+chmod +x /etc/systemd/system/odroid-config-settings.service
+systemctl enable odroid-config-settings &&
+systemctl restart odroid-config-settings
